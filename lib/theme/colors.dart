55a894195636d7880b27a7649d747d5f9bc54d1b
FlutLab.io <>
2021-04-22 08:16:39 +0000

    Initial commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF0077B6);
+// const Color primary = Color(0xFF04be4e);
+const Color black = Color(0xFF4EA8DE);
+const Color white = Color(0xFFFFFFFF);
+const Color grey = Color(0xFF5f5f5f);
